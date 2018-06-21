124d43132a1b8af5e32212133e5ae183b007ce97
Lam Tran <lam.tran10@gmail.com>
2018-06-21 15:15:37 -0700

    updates

--- app.js
+++ app.js
@@ -28,9 +28,6 @@ app.get('/contact', function(req, res){
 	res.sendFile(__dirname+'/contact.html');
 });
 
-app.get('/hello', function(req, res){
-	res.send('<h1>Hello Javascript Developer</h1>');
-});
 
 app.listen(3000, function(){
 	console.log("The application is running");
